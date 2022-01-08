5212cbd19f17617634e3bb837c31861a3f86636b
charlene <charlcpy@gmail.com>
2022-01-08 11:46:19 +0800

    Initial Commit

---
+++ ios-exercise/ios_exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_exerciseApp.swift
+//  ios-exercise
+//
+//  Created by yan on 8/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
