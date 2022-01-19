c88a8d7c044a9cab7a7164ca8d8547d541ec7257
Charlene Cheung <charlenecheung@hket.com>
2022-01-19 10:01:26 +0800

    add method

--- calculator/calculator/viewModel/calculatorViewModel.swift
+++ calculator/calculator/viewModel/calculatorViewModel.swift
@@ -15,5 +15,42 @@ class CalculatorViewMode {
         [.one, .two, .three, .adding],
         [.reverse, .zero, .dot, .equal]
     ]
+    
+    func add(val1: Double, val2: Double) -> String {
+        return "\(val1 + val2)"
+    }
+    
+    func subtract(val1: Double, val2: Double) -> String {
+        return "\(val1 - val2)"
+    }
+    
+    func multipy(val1: Double, val2: Double) -> String {
+        return "\(val1 * val2)"
+    }
+    
+    func divide(val1: Double, val2: Double) -> String {
+        return "\(val1 / val2)"
+    }
+    
+    func toPercentage(val1: Double) -> String {
+        return "\(val1 / 100)"
+    }
+    
+    func toOpposeValue(val1: Double) -> String {
+        return "\(val1 / -1)"
+    }
+    
+    func toFloatingPoint(val1: Double) -> String {
+        return "\(val1)"
+//        todo fix
+    }
+    
+    func clear() {
+//        todo
+    }
+    
+    func calculate() {
+        // todo
+    }
 
 }
